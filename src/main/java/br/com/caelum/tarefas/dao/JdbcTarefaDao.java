7dd7fa18a18cb801b3a9d0e5ae94a2429dbac58e
Paulo Grabin <plgrabin@gmail.com>
2026-04-13 22:18:54 -0300

    Remove unused import

--- src/main/java/br/com/caelum/tarefas/dao/JdbcTarefaDao.java
+++ src/main/java/br/com/caelum/tarefas/dao/JdbcTarefaDao.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
-import javax.persistence.Entity;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
